6dee234384b271e93c073f70cd9a9785cf60ffd5
LightWat <dennislai120@gmail.com>
2022-12-03 22:23:12 +0800

    merge

--- Assets/Scripts/GameManager/LevelSystem.cs
+++ Assets/Scripts/GameManager/LevelSystem.cs
@@ -6,11 +6,13 @@ public class LevelSystem {
   private int _level;
   private int _experience;
   private int _experienceToNextLevel;
+  private int _experienceOffset;
 
   public LevelSystem() {
     _level = 0;
     _experience = 0;
     _experienceToNextLevel = 100;
+    _experienceOffset = 10;
   }
 
   public void AddExperience(int amount) {
@@ -20,7 +22,8 @@ public class LevelSystem {
       if (_experience >= _experienceToNextLevel) {
         _level++;
         _experience -= _experienceToNextLevel;
-
+        _experienceToNextLevel += _experienceOffset;
+        
         if (_level == 9) _experience = 0;
       }
     }
