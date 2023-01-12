162e8d2a66bac544e4392fa0d278c415990332c9
kylai02 <dennislai120@gmail.com>
2023-01-12 10:39:31 +0800

    fix

--- Assets/Scripts/Building/T4~0/T1.cs
+++ Assets/Scripts/Building/T4~0/T1.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 class T1 : Building {
   private bool _alreadyAlert;
-  
+  private bool _isUpgrade;
+  public bool _canUpgrade;
+
+  private void Awake() {
+    _canUpgrade = Random.Range(0, 10) == 0;
+  }
+
   private void Update() {
     OnCorrodeCheck();
     OnFireCheck();
@@ -22,8 +28,22 @@ class T1 : Building {
     _alreadyAlert = false;
   }
 
+  protected override void Upgrade() {
+    if (_canUpgrade && _timer >= countdown) {
+      experience = 0;
+
+      GameObject nextStage = Instantiate(
+        nextStage1,
+        transform.position,
+        nextStage1.transform.rotation
+      );
+
+      Destroy(wholeObject);
+    }
+  }
+
   private void ShowDangerous() {
-    if (!_alreadyAlert) {
+    if (_canUpgrade && !_alreadyAlert) {
       upgradeEffect.SetActive(true);
       GameObject.Find("GameManager").GetComponent<GameManager>().Alert();
       GameObject.Find("GameManager").GetComponent<GameManager>().AlertCounterChange(1);
